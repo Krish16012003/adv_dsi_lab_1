711ba8801ad3f1bbb7dc37eeb4a777aece062157
Krish16012003 <shahkrish1613@gmail.com>
2024-09-22 11:48:04 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning engineering lab project for data science workflows.',
+    author='Krish16012003',
+    license='MIT',
+)
